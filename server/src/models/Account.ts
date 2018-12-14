115af1fb72a42481214a55c7f2a8c444f091e126
ipinlnd <nlndipi@hotmail.com>
2018-12-14 13:39:05 +0330

    Added Model for account using sequelize-typescript

---
+++ server/src/models/Account.ts
@@ -0,0 +1,22 @@
+import {Table, Column, Model, PrimaryKey, Length, DataType, AutoIncrement, AllowNull} from 'sequelize-typescript'
+
+@Table
+export class Account extends Model<Account>
+{
+	@PrimaryKey
+	@AutoIncrement
+	@Column
+	id: number;
+
+	@Length({min: 3, max: 10})
+	@Column
+	name: String;
+
+	@Length({min: 6, max: 10})
+	@Column
+	password: String;
+
+	@AllowNull
+	@Column(DataType.TEXT)
+	motto: String;
+}
