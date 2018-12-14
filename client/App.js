490d79c34b4a05e279ba8fcfc1ed48b40daddcf3
ipinlnd <nlndipi@hotmail.com>
2018-12-14 15:24:45 +0330

    Added apollo provider to App.js to connect to the server

--- client/App.js
+++ client/App.js
@@ -3,6 +3,10 @@ import { View } from 'react-native';
 import { Font } from 'expo'
 import { Root } from "native-base"
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
 
 import { Login } from './components/Login'
 import { Main } from './components/Main'
@@ -18,6 +22,17 @@ const RootStack = createStackNavigator(
 })
 
 const AppContainer = createAppContainer(RootStack)
+
+const httpLink = createHttpLink(
+{
+	uri: 'http://192.168.1.53:4000'
+})
+
+const client = new ApolloClient(
+{
+	link: httpLink,
+	cache: new InMemoryCache()
+})
 
 export default class App extends React.Component
 {
@@ -46,9 +61,11 @@ export default class App extends React.Component
 		if (!this.state.loaded) 
 			return (<View></View>)
 		return (
-				<Root>
+			<Root>
+				<ApolloProvider client={client}>
 					<AppContainer />
-				</Root>
+				</ApolloProvider>
+			</Root>
 		);
 	}
 }
