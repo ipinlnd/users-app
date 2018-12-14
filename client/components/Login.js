79df05d9c5f836abe1caf0eb197d1cb047309301
ipinlnd <nlndipi@hotmail.com>
2018-12-14 15:19:20 +0330

    Added Login screen source file

---
+++ client/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export class Login extends React.Component
+{
+	render()
+	{
+		return(
+			<View>
+				<Text>
+					Login page
+				</Text>
+			</View>
+		)
+	}
+}
